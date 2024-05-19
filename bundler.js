af0d6c93f6cac8b5404c3b423c433ebff61fc325
croissant <wathyic@gmail.com>
2024-05-19 21:35:19 +0800

    first commit

---
+++ bundler.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+const babel = require("@babel/core");
+
+const moduleAnalyser = (filename) => {
+  const content = fs.readFileSync(filename, "utf-8");
+  console.log(content);
+  const ast = parser.parse(content, {
+    sourceType: "module",
+  }); // AST
+  // console.log(ast);
+  // console.log(ast.program.body);
+};
+
+const moduleInfo = moduleAnalyser("./src/index.js");
